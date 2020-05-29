8a45723cddf5214c3100a965590d0268d99f1761
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-05-29 16:14:05 -0300

    Fixs

--- front/src/components/Pages/Footer.jsx
+++ front/src/components/Pages/Footer.jsx
@@ -41,23 +41,29 @@ const Footer = ({ heightOnPage }) => {
     <section id='footer' className='footer-container container notAnim'>
       <div className='row'>
         <div className="col-md-6 row">
-          <div className="col-md-6 col-6">
-            <h3 className='footer-group-title' >Página Inicial</h3>
+          <div className="col-md-6 col-4">
+            <h3 className='footer-group-title' >Início</h3>
             <a className='footer-link-a' onClick={() => hashHistory.push('/')} ><MdKeyboardArrowRight size={25} /> Home</a>
           </div>
-          <div className="col-md-6 col-6">
-            <h3 className='footer-group-title' >Serviços</h3>
+          <div className="col-md-6 col-8">
+            <h3 className='footer-group-title' style={{textAlign: 'center'}}>Serviços</h3>
             <div className="footer-group-link">
-              <a className='footer-link-a' onClick={() => hashHistory.push('/page1')} ><MdKeyboardArrowRight size={25} /> Page 1</a>
-              <a className='footer-link-a' onClick={() => hashHistory.push('/page2')} ><MdKeyboardArrowRight size={25} /> Page 2</a>
-              <a className='footer-link-a' onClick={() => hashHistory.push('/page3')} ><MdKeyboardArrowRight size={25} /> Page 3</a>
-              <a className='footer-link-a' onClick={() => hashHistory.push('/page4')} ><MdKeyboardArrowRight size={25} /> Page 4</a>
-              <a className='footer-link-a' onClick={() => hashHistory.push('/pageX')} ><MdKeyboardArrowRight size={25} /> Page X</a>
+              <div className="row">
+                <a className='footer-link-a' onClick={() => hashHistory.push('/page1')} ><MdKeyboardArrowRight size={25} /> Page 1</a>
+                <a className='footer-link-a' onClick={() => hashHistory.push('/page4')} ><MdKeyboardArrowRight size={25} /> Page 4</a>
+              </div>
+              <div className="row">
+                <a className='footer-link-a' onClick={() => hashHistory.push('/page2')} ><MdKeyboardArrowRight size={25} /> Page 2</a>
+                <a className='footer-link-a' onClick={() => hashHistory.push('/pageX')} ><MdKeyboardArrowRight size={25} /> Page X</a>
+              </div>
+              <div className="row">
+                <a className='footer-link-a' onClick={() => hashHistory.push('/page3')} ><MdKeyboardArrowRight size={25} /> Page 3</a>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-6 footer-group-redes">
-          <h3 className='footer-group-title' >Siga-nos em nossas Redes Sociáis</h3>
+          <h3 className='footer-group-title' >Siga-nos em nossas Redes Sociais</h3>
 	  <div className='footer-group-redes-container col-md-9'>
 	  <ul className='footer-group-redes-ul'>
 	    <li className='footer-group-redes-li'><a className='footer-group-redes-a' href="#"> <FaFacebookF className='footer-group-rede-icon' /> </a></li>
